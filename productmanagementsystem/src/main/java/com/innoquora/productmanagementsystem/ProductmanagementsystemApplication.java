545133f681fddfd3437d5afab99d154c30a9b7db
admin <sachinsharma88786@gmail.com>
2026-09-10 12:30:38 +0530

    this is initial comment

--- productmanagementsystem/src/main/java/com/innoquora/productmanagementsystem/ProductmanagementsystemApplication.java
+++ productmanagementsystem/src/main/java/com/innoquora/productmanagementsystem/ProductmanagementsystemApplication.java
@@ -9,9 +9,7 @@ public class ProductmanagementsystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductmanagementsystemApplication.class, args);
 		System.out.println("Hii guys");
-		System.out.println("Hello guys");
-		System.out.println("Goodbye guys");
-		System.out.println("hii this is end line of my code");
+		System.out.println("Hello this is my first application");
 	}
 
 }
