c8b478e3f154d276d42bcf721124e4150af2e667
admin <sachinsharma88786@gmail.com>
2026-09-11 22:34:02 +0530

    this is my testing update

--- productmanagementsystem/src/main/java/com/innoquora/productmanagementsystem/ProductmanagementsystemApplication.java
+++ productmanagementsystem/src/main/java/com/innoquora/productmanagementsystem/ProductmanagementsystemApplication.java
@@ -9,14 +9,7 @@ public class ProductmanagementsystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductmanagementsystemApplication.class, args);
 		System.out.println("Hii guys");
-		System.out.println("Hello this is my first application");
-		System.out.println("hfdjhfdjshfjdsfhds");
-		System.out.println("fsdkfjdskfhjdskfhdffdsfdsf");
-		System.out.println("this is last line");
-		System.out.println("hiii this is my project file");
-		System.out.println("hii my name is arjun patel");
-		System.out.println("hii my name is gopal tirole");
-		System.out.println("hii my name is gopal tirole");
+
 
 	}
 
